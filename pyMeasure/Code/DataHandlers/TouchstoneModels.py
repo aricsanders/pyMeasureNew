909912042bd3c742b4e8ca60833fd79f7766fd1c
aricsanders <aric.sanders@gmail.com>
2016-03-07 01:16:59 -0700

    Data Files in Tests Folder

--- pyMeasure/Code/DataHandlers/TouchstoneModels.py
+++ pyMeasure/Code/DataHandlers/TouchstoneModels.py
@@ -43,7 +43,33 @@ TOUCHSTONE_KEYWORDS=["Version","Number of Ports","Two-Port Order","Number of Fre
 
 #-----------------------------------------------------------------------------
 # Module Classes
-
+class s2pv1(AsciiDataTable):
+    """A container for s2p files"""
+    def __init__(self,file_path=None,**options):
+        """Initialization of the s2p class for version 1 files,
+        if a file path is specified opens the file. If the file path is not
+        specified then data can be added through the s2pv1.data. It automatically changes whitespace to a single space."""
+        defaults={"data_delimiter":" ",
+                  "column_names_delimiter":None,
+                  "specific_descriptor":'Data',
+                  "general_descriptor":'Table',
+                  "directory":None,
+                  "extension":'s2p',
+                  "comment_begin":"!",
+                  "comment_end":"\n",
+                  "inline_comment_begin":None,
+                  "inline_comment_end":None,
+                  "block_comment_begin":None,
+                  "block_comment_end":None,
+                  "footer_begin_line":None,
+                  }
+        self.options={}
+        for key,value in defaults.iteritems():
+            self.options[key]=value
+        for key,value in options.iteritems():
+            self.options[key]=value
+        self.elements=['header','column_names','data','footer','inline_comments']
+        AsciiDataTable.__init__(self,file_path,**options)
 #-----------------------------------------------------------------------------
 # Module Scripts
 
